5c3d3b228123e022a2d32c5a58f3bedd3edf4d91
mathmatowicki <matowicki@gmail.com>
2020-06-01 15:32:42 +0200

    Add function that display images of numbers

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import os
 import struct
 import numpy as np
@@ -20,3 +21,13 @@ X_train, y_train = load_mnist('mnist', kind='train')
 print('Rows: %d, columns: %d' % (X_train.shape[0], X_train.shape[1]))
 X_test, y_test = load_mnist('mnist', kind='t10k')
 print('Rows: %d, columns: %d' % (X_test.shape[0], X_test.shape[1]))
+
+fig, ax = plt.subplots(nrows=2, ncols=5, sharex=True, sharey=True,)
+ax = ax.flatten()
+for i in range(10):
+    img = X_train[y_train == i][0].reshape(28, 28)
+    ax[i].imshow(img, cmap='Greys', interpolation='nearest')
+ax[0].set_xticks([])
+ax[0].set_yticks([])
+plt.tight_layout()
+plt.show()
