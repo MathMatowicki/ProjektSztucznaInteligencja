91c2e0a8c2c8c3d9065ce8fb5dcc9d395e61a344
mathmatowicki <matowicki@gmail.com>
2020-06-01 16:39:03 +0200

    Add plots of Epchos and evaluate the performance of the model

--- script.py
+++ script.py
@@ -192,3 +192,38 @@ nn = NeuralNetMLP(n_output=10, n_features=X_train.shape[1], n_hidden=50, l2=0.1,
                   minibatches=50,
                   random_state=1)
 nn.fit(X_train, y_train, print_progress=True)
+
+plt.plot(range(len(nn.cost_)), nn.cost_)
+plt.ylim([0, 2000])
+plt.ylabel('Cost')
+plt.xlabel('Epochs * 50')
+plt.tight_layout()
+plt.show()
+
+batches = np.array_split(range(len(nn.cost_)), 1000)
+cost_ary = np.array(nn.cost_)
+cost_avgs = [np.mean(cost_ary[i]) for i in batches]
+plt.plot(range(len(cost_avgs)), cost_avgs, color='red')
+plt.ylim([0, 2000])
+plt.ylabel('Cost')
+plt.xlabel('Epochs')
+plt.tight_layout()
+plt.show()
+
+y_train_pred = nn.predict(X_train)
+acc = np.sum(y_train == y_train_pred, axis=0) / X_train.shape[0]
+print('Training accuracy: %.2f%%' % (acc * 100))
+
+miscl_img = X_test[y_test != y_test_pred][:25]
+correct_lab = y_test[y_test != y_test_pred][:25]
+miscl_lab = y_test_pred[y_test != y_test_pred][:25]
+fig, ax = plt.subplots(nrows=5, ncols=5, sharex=True, sharey=True,)
+ax = ax.flatten()
+for i in range(25):
+    img = miscl_img[i].reshape(28, 28)
+    ax[i].imshow(img, cmap='Greys', interpolation='nearest')
+    ax[i].set_title('%d) t: %d p: %d' % (i+1, correct_lab[i], miscl_lab[i]))
+ax[0].set_xticks([])
+ax[0].set_yticks([])
+plt.tight_layout()
+plt.show()
